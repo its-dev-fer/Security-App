e435b045733c0b22aeb7fe234744c0f69fedfbf5
Luis Fernando Hernández Morales <163189@ids.upchiapas.edu.mx>
2018-03-13 12:41:44 -0600

    ...
    ...

--- TuxAlert/js/TuxAlert.js
+++ TuxAlert/js/TuxAlert.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+
+	//Checar si es la primera vez que se abre la app
+	var primeraVez = localstorage.get('primeraVez');
+	if(primeraVez==null){
+		localstorage.set('primeraVez',1);
+		location.href = "#tarjeta-1";
+	}else{
+		location.href = "#inicio";
+	}
+
 	$("#goHome").click(function(e){
 		e.preventDefault();
 		location.href = "#inicio";
