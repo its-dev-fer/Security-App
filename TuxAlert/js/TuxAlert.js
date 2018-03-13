64d593470574a78180f3aef2ab662869cfced150
elticha <163189@ids.upchiapas.edu.mx>
2018-03-13 14:36:05 -0600

    Bandera de primera vez añadida
    La app ya reconoce cuándo la app se está ejecutando por primera vez y cupando no.

--- TuxAlert/js/TuxAlert.js
+++ TuxAlert/js/TuxAlert.js
@@ -1,22 +1,16 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-
 	//Checar si es la primera vez que se abre la app
-	var primeraVez = localstorage.get('primeraVez');
-	if(primeraVez==null){
-		localstorage.set('primeraVez',1);
-		location.href = "#tarjeta-1";
+	var primeraVez = localStorage.getItem('primeraVez',null);
+	if(!primeraVez){
+		localStorage.setItem('primeraVez',1);
 	}else{
 		location.href = "#inicio";
 	}
-=======
-	
-	location.href = "#inicio";
->>>>>>> 6ea3b6082271b081ff83a8e0a13a8574ef18b143
 
 	$("#goHome").click(function(e){
 		e.preventDefault();
 		location.href = "#inicio";
+		$("#goHome").attr('data-transition','flip');
 	});
 
 	$("#signup").click(function(e){
