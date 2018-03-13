9f804008e3f253df2b95cd6f4b5a36a4796b5746
elticha <163189@ids.upchiapas.edu.mx>
2018-03-13 15:27:58 -0600

    Corrección de errores minúsculos
    ...

--- TuxAlert/js/TuxAlert.js
+++ TuxAlert/js/TuxAlert.js
@@ -1,16 +1,55 @@
 $(document).ready(function(){
+
+	//Validar que el usario esté conectado a internet
+	if(!navigator.onLine){
+	  	alert('Necesitamos conexión a Internet para ofrecerte nuestro servicio :c');
+  		navigator.app.exitApp();
+ 	}
+
+
 	//Checar si es la primera vez que se abre la app
 	var primeraVez = localStorage.getItem('primeraVez',null);
 	if(!primeraVez){
-		localStorage.setItem('primeraVez',1);
+		//localStorage.setItem('primeraVez',1);
+		location.href = "#tarjeta-1"
 	}else{
 		location.href = "#inicio";
 	}
 
+	/* ======================================================= */
+
+	//Asignar un ID a cada botón de las tarjetas de presentación
+	$("#btn-tarjeta-1").click(function(e){
+		e.preventDefault();
+		location.href =  "#tarjeta-1";
+	});
+
+	$("#btn-tarjeta-2").click(function(e){
+		e.preventDefault();
+		location.href =  "#tarjeta-2";
+	});
+
+	$("#btn-tarjeta-3").click(function(e){
+		e.preventDefault();
+		location.href =  "#tarjeta-3";
+	});
+
+	$("#btn-tarjeta-4").click(function(e){
+		e.preventDefault();
+		location.href =  "#tarjeta-4";
+	});
+
+	$("#btn-tarjeta-5").click(function(e){
+		e.preventDefault();
+		location.href =  "#tarjeta-5";
+	});
+
+	/* ======================================================= */
 	$("#goHome").click(function(e){
 		e.preventDefault();
 		location.href = "#inicio";
 		$("#goHome").attr('data-transition','flip');
+		localStorage.setItem('primeraVez',1);
 	});
 
 	$("#signup").click(function(e){
