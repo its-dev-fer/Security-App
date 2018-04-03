1b9477ef33c04ec1cc832e4fdd882c35d768f989
Kvothe <charlyandy201088@gmail.com>
2018-04-03 15:02:00 -0500

    Nuevo código de Geo que no sirve pero se mira más bonitoxd

--- TuxAlert/js/TuxAlert.js
+++ TuxAlert/js/TuxAlert.js
@@ -3,41 +3,36 @@
 $(document).ready(function(){
 
 	//gelocation aki pq soy mui cul
-		
+	function GoogleMap(position) {
+	  var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+	  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+	    zoom: 20,
+	    disableDefaultUI: true,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+	  });
+
+	  var marker = new google.maps.Marker({
+	    map: map,
+	    position: location,
+	    animation: google.maps.Animation.DROP,
+	    title: "This is your location"
+	  });
+
+	  map.setCenter(location);
+	}
+
+	function showError() {
+	  alert("Location can't be found");
+	}
+
    $(document).on( "pageinit", "#mapaVista", function(e) {
-	   	e.preventDefault();
-	//	   	alert("yipiyaiyo");
-    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
-    if ( navigator.geolocation ) {
-        function success(pos) {
-            // te encontramos prro, dibuja el puto mapa
-            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-			//alert("latitud:" + pos.coords.latitude+ " longitud: " + pos.coords.longitude);
-        }
-        function fail(error) {
-            drawMap(defaultLatLng);  // onde bergastas, dibuja un mapa default			
-			
-        }
-        // te busca prro, guarda en cashe 5min, intenta por 6 segundos
-        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
-    } else {
-        drawMap(defaultLatLng);  // nel prro no te vengo manejando los mapas.
-    }
-    function drawMap(latlng) {
-        var myOptions  = {
-            zoom: 10,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        //dibuja sobre el pinshimaap
-        var marker = new google.maps.Marker({
-            position: latlng,
-            map: map,
-            title: "kp2!"
-		});
-       }
-	    
+   		e.preventDefault();
+   		if (navigator.geolocation) {
+		  navigator.geolocation.getCurrentPosition(GoogleMap, showError);
+		} else {
+		  alert("Your browser does not support Geolocation.");
+		}
 	});
 
 	//fin geolocation
