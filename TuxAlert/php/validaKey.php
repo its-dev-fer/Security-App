f3e1ff6ea9c0af29d8f5ed65748b9cbcf5efec4b
José Ramón Palacios Martínez <37125152+Raymond981@users.noreply.github.com>
2018-04-14 16:09:41 -0500

    Valida key si existe o no, 98% completado.
    Solo falta la alerta de que la key no existe, o ya se utilizo anteriormente

--- TuxAlert/php/validaKey.php
+++ TuxAlert/php/validaKey.php
@@ -10,36 +10,19 @@
       $stmt->execute();
 
       while ($datos = $stmt->fetch()) {
-        if($claveIngresada == $datos[1]){
+        if($claveIngresada == $datos[1] && $datos[2] == 1){
           $existeClave = true;
         }
       }
 
       if($existeClave == true){
-        header("location: http://127.0.0.1/index.html#pantallaPrincipal");
-      }
-
-      $con = null;
- ?>
-
-<?php
-      $claveIngresada = $_POST['claveUsuario'];
-      $existeClave = false;
-
-      $con = new PDO('mysql:host=localhost;dbname=tuxalert_app_db', 'root',"", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-      $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      $stmt = $con->prepare('SELECT * FROM `keys`');
-      $stmt->execute();
-
-      while ($datos = $stmt->fetch()) {
-        if($claveIngresada == $datos[1]){
-          $existeClave = true;
+        try{
+            $stmt = $con->prepare('UPDATE `keys` SET `estado`=0 WHERE `key`= "'.$claveIngresada.'"');
+            $stmt->execute();
+            header("location: http://127.0.0.1/index.html#registrosp");
+        } catch (PDOException $e) {
+          echo 'Error: '. $e->getMessage();
         }
-      }
-
-      if($existeClave == true){
-        header("location: http://127.0.0.1/index.html#pantallaPrincipal");
       }
 
       $con = null;
