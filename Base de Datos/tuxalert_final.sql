44d6822ee095ff3d2e0432f136fe6c04c5c15123
Luis Fernando Hernández Morales <163189@ids.upchiapas.edu.mx>
2018-04-16 17:59:18 -0500

    New DB structure

--- Base de Datos/tuxalert_final.sql
+++ Base de Datos/tuxalert_final.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 100131
  Source Host           : localhost:3306
- Source Schema         : tuxalert_final
+ Source Schema         : tuxalert_app_db
 
  Target Server Type    : MySQL
  Target Server Version : 100131
  File Encoding         : 65001
 
- Date: 14/04/2018 11:08:06
+ Date: 16/04/2018 17:58:56
 */
 
 SET NAMES utf8mb4;
@@ -22,13 +22,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `alertas`;
 CREATE TABLE `alertas`  (
-  `ID_Alerta` int(255) NOT NULL,
+  `ID_Alerta` int(255) NOT NULL AUTO_INCREMENT,
   `Fecha` date NULL DEFAULT NULL,
-  `Hora` varbinary(255) NULL DEFAULT NULL,
+  `Hora` datetime(6) NULL DEFAULT NULL,
   `Latitud` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   `Longitud` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   PRIMARY KEY (`ID_Alerta`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_spanish2_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8 COLLATE = utf8_spanish2_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for keys
@@ -51,12 +51,16 @@ CREATE TABLE `usuarios`  (
   `Apellidos` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   `Correo_electronico` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   `Fecha_nacimiento` date NULL DEFAULT NULL,
-  `Telefono` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
+  `Telefono` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   `Contrasenia` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   `Genero` enum('masculino','femenino','otro') CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
   `Borrado` int(1) NULL DEFAULT NULL,
   `ID_TipoUsuario` int(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID_Usuario`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_spanish2_ci ROW_FORMAT = Compact;
+  `Activo` int(255) NULL DEFAULT NULL,
+  `Ultima_sesion` timestamp(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID_Usuario`) USING BTREE,
+  UNIQUE INDEX `unique_Correo`(`Correo_electronico`) USING BTREE,
+  UNIQUE INDEX `unique_Telefono`(`Telefono`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_spanish2_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
